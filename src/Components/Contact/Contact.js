ba047fd216c0bedd8e4752296eb0cd70c92fe873
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-08-12 11:01:27 -0700

    added email

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Contact.css'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -6,33 +6,29 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import { IconButton } from '@material-ui/core';
 import Aos from 'aos';
 import "aos/dist/aos.css"
-import { db } from './firebase'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [loader, setLoader] = useState(false)
+    const form = useRef();
 
     useEffect(() => {
         Aos.init({ duration: 3000 });
     }, [])
 
     const handleSubmit = (e) => {
+        console.log("Submitting...")
         e.preventDefault();
         setLoader(true);
-        //Send to Firebase database
-        db.collection("contacts").add({
-            name: name,
-            email: email,
-            message: message
-        })
-            .then(() => {
-                alert("Your Message has been submitted")
+        emailjs.sendForm('service_u77wwqy', 'template_0iowr16', form.current, 'SY_LCGri0AM_EYC9x')
+            .then((result) => {
+                console.log(result.text);
                 setLoader(false)
-            })
-            .catch((error) => {
-                alert(error.message);
+            }, (error) => {
+                console.log(error.text);
                 setLoader(false)
             });
         //Reset form
@@ -45,22 +41,22 @@ function Contact() {
         <div className="contact" id="section__contact">
             <div className="contact__box">
                 <h1>LET'S TALK!</h1>
-                <form className="contact__form" data-aos="fade">
-                    <label>Name</label>
-                    <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+                <form ref={form} className="contact__form" data-aos="fade" onSubmit={handleSubmit}>
+                    <label>Your Name</label>
+                    <input value={name} onChange={(e) => setName(e.target.value)} name="user_name" placeholder="Name" />
 
                     <label>Email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="user_email" placeholder="email" />
 
                     <label>Message</label>
-                    <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" />
-                    <button onClick={handleSubmit} style={{ background: loader ? "#ccc" : "#FC4445" }} type="submit"> SUBMIT </button>
+                    <textarea value={message} onChange={(e) => setMessage(e.target.value)} name="message" placeholder="message" />
+                    <button style={{ background: loader ? "#ccc" : "#FC4445" }} type="submit"> SUBMIT </button>
                 </form>
                 <div class="contact__bottom">
                     <div class="contact__invisible">
 
                     </div>
-                    <p> alternatively, email me at pac469@nyu.edu</p>
+                    <p> alternatively, email me at paulin.alcoser@nyu.edu</p>
                     <div className="contact__icons">
                         <a href="https://www.linkedin.com/in/paulin-alcoser-chong-824597197/" target="_blank">
                             <IconButton ><LinkedInIcon className="contact__icon" id="linkedIn" fontSize="Large" /></IconButton>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
         <div className="home" id="section__home">
             <div className="home__box">
                 <h1 className="home__boxName">Paulin <span>Alcoser</span></h1>
-                <h1 className="home__boxRole">Web Developer.</h1>
+                <h1 className="home__boxRole">Software Engineer</h1>
             </div>
         </div>
     )
